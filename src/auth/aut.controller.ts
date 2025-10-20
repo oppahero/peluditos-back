4ef4366b05e237ca9610b816844bfae8ec553ad0
Maria <beadleshero@gmail.com>
2025-10-20 13:20:51 -0400

    feat: add swagger documentation for login endpoint

--- src/auth/aut.controller.ts
+++ src/auth/aut.controller.ts
@@ -2,14 +2,30 @@ import { Controller, Post, Request, UseGuards } from '@nestjs/common';
 import { UserResponseDto } from 'src/modules/users/dto/users.dto';
 import { LocalAuthGuard } from './guards/local-auth.guard';
 import { AuthService } from './auth.service';
+import { ApiBody, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { LoginDto } from './dto/login.dto';
 
+@ApiTags('auth')
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
+  /**
+   *
+   * @returns {access_token: string} Devuelve token de autentización
+   * @param {Request} res Datos del usuario
+   */
   @Post('login')
+  @ApiOperation({ summary: 'Autenticación de usuario' })
+  @ApiBody({ type: LoginDto })
+  @ApiResponse({
+    status: 200,
+    description: 'Token de acceso generado correctamente',
+    example: {
+      access_token: 'lkaskanklsnalkññalkasjkjalks',
+    },
+  })
   @UseGuards(LocalAuthGuard)
-  // @UseGuards(AuthGuard('local'))
   async login(@Request() req: { user: UserResponseDto }) {
     return this.authService.login(req.user);
   }
--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -1,13 +1,14 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString } from 'class-validator';
 
 export class LoginDto {
   @IsString()
-  @IsNotEmpty()
   @IsNotEmpty({ message: 'Username is required' })
+  @ApiProperty({ example: 'mlopez707', description: 'Nombre de usuario' })
   username: string;
 
   @IsString()
-  @IsNotEmpty()
+  @ApiProperty({ example: '123456', description: 'Contraseña del usuario' })
   @IsNotEmpty({ message: 'Password is required' })
   password: string;
 }
