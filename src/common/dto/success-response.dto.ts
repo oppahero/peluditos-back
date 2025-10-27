13287ced6b3d61d4577074000e670951dc22d30c
Maria <beadleshero@gmail.com>
2025-10-26 21:56:25 -0400

    docs: add Swagger annotation to SuccessResponseDto

--- src/common/dto/success-response.dto.ts
+++ src/common/dto/success-response.dto.ts
@@ -1,6 +1,13 @@
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+
 export class SuccessResponseDto<T> {
+  @ApiProperty({ example: true })
   success = true;
+
+  @ApiPropertyOptional({ example: 'Mensaje' })
   message?: string;
+
+  @ApiPropertyOptional({ example: {} })
   data?: T;
 
   constructor(init?: Partial<SuccessResponseDto<T>>) {
