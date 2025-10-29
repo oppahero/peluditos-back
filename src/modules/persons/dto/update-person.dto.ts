03d45928e4f37630d48f60e3804685f2cd5eb87e
Maria <beadleshero@gmail.com>
2025-10-29 09:59:38 -0400

    refactor: mark fields as optional in UpdatePersonDto

--- src/modules/persons/dto/update-person.dto.ts
+++ src/modules/persons/dto/update-person.dto.ts
@@ -1,25 +1,29 @@
 import { ApiProperty, PartialType } from '@nestjs/swagger';
 import { CreatePersonDto } from './create-person.dto';
-import { IsString, MaxLength } from 'class-validator';
+import { IsOptional, IsString, MaxLength } from 'class-validator';
 
 export class UpdatePersonDto extends PartialType(CreatePersonDto) {
   @IsString()
   @MaxLength(30)
   @ApiProperty({ example: 'Paola López' })
+  @IsOptional()
   name?: string;
 
   @IsString()
   @MaxLength(11)
   @ApiProperty({ example: '04121939372' })
+  @IsOptional()
   phone?: string;
 
   @IsString()
   @MaxLength(15)
   @ApiProperty({ example: 'paola@gmail.com' })
+  @IsOptional()
   email?: string;
 
   @IsString()
   @MaxLength(15)
   @ApiProperty({ example: 'Alta Vista' })
+  @IsOptional()
   address?: string;
 }
