a05a1f0087755e9374e48b8ba3032e09fea07ae7
Maria <beadleshero@gmail.com>
2025-10-29 09:58:47 -0400

    refactor: mark fields as optional in UpdateTypeOfServiceDto

--- src/modules/types-of-services/dto/update-type-of-service.dto.ts
+++ src/modules/types-of-services/dto/update-type-of-service.dto.ts
@@ -1,21 +1,31 @@
 import { ApiProperty, PartialType } from '@nestjs/swagger';
 import { CreateTypeOfServiceDto } from './create-type-of-service.dto';
-import { IsBoolean, IsNumber, IsString, MaxLength, Min } from 'class-validator';
+import {
+  IsBoolean,
+  IsNumber,
+  IsOptional,
+  IsString,
+  MaxLength,
+  Min,
+} from 'class-validator';
 
 export class UpdateTypeOfServiceDto extends PartialType(
   CreateTypeOfServiceDto,
 ) {
   @IsString()
   @MaxLength(20)
+  @IsOptional()
   @ApiProperty({ example: 'Consulta Veterinaria' })
   description?: string;
 
   @IsNumber()
   @Min(1)
+  @IsOptional()
   @ApiProperty({ example: 20 })
   price?: number;
 
   @IsBoolean()
+  @IsOptional()
   @ApiProperty({ example: true })
   medical_assistance?: boolean;
 }
