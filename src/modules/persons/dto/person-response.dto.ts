d385b999102ee80d6a65f97cc8fa7a10b3b866f8
Maria <beadleshero@gmail.com>
2025-11-02 13:25:26 -0400

    refactor(persons): enhance PersonResponseDto with additional properties and expose decorators

--- src/modules/persons/dto/person-response.dto.ts
+++ src/modules/persons/dto/person-response.dto.ts
@@ -1,20 +1,28 @@
-import { ApiProperty } from '@nestjs/swagger';
+import { ApiProperty, OmitType } from '@nestjs/swagger';
+import { Expose, Transform, Type } from 'class-transformer';
 import { TypesOfTaxpayer } from 'src/common/enums/types-of-taxpayer.enum';
+import { EmployeeEmbeddedDto } from 'src/modules/employees/dto/employee-response.dto';
+import { NaturalPersonEmbeddedDto } from 'src/modules/natural-persons/dto/natural-person-response.dto';
 
 export class PersonResponseDto {
   @ApiProperty({ example: 99 })
+  @Expose()
   readonly persons_id: number;
 
   @ApiProperty({ example: 'Paola López' })
+  @Expose()
   readonly name: string;
 
   @ApiProperty({ example: '04121939372' })
+  @Expose()
   readonly phone: string;
 
   @ApiProperty({ example: 'paola@gmail.com' })
+  @Expose()
   readonly email: string;
 
   @ApiProperty({ example: 'Alta Vista' })
+  @Expose()
   readonly address: string;
 
   @ApiProperty({
@@ -22,5 +30,28 @@ export class PersonResponseDto {
     enum: TypesOfTaxpayer,
     description: 'Tipo de Contribuyente',
   })
+  @Expose()
   readonly taxpayer_type: TypesOfTaxpayer;
+}
+
+export class PersonEmbeddedDto extends OmitType(PersonResponseDto, [
+  'persons_id',
+] as const) {}
+
+export class PersonWithRelationsResponseDto extends PersonResponseDto {
+  @ApiProperty({ type: NaturalPersonEmbeddedDto, required: false })
+  @Type(() => NaturalPersonEmbeddedDto)
+  @Expose()
+  @Transform(({ value }) => (value === undefined ? undefined : value), {
+    toPlainOnly: true,
+  })
+  naturalPerson?: NaturalPersonEmbeddedDto;
+
+  @ApiProperty({ type: EmployeeEmbeddedDto, required: false })
+  @Type(() => EmployeeEmbeddedDto)
+  @Expose()
+  @Transform(({ value }) => (value === undefined ? undefined : value), {
+    toPlainOnly: true,
+  })
+  employee?: EmployeeEmbeddedDto;
 }
